56da83bb1ca9682f244ed71677922acf8f611894
Shree Krishna Yadav <108301606+mykks32@users.noreply.github.com>
2025-12-05 23:28:24 +0545

    Refactor: Update config.ts

--- src/config/config.ts
+++ src/config/config.ts
@@ -64,7 +64,3 @@ export class ConfigService {
 // Export direct instance for convenience
 export const config = ConfigService.getInstance();
 export const getConfig = () => ConfigService.getInstance();
-
-(async () => {
-    ConfigService.logValidation();
-})();
